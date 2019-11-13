0a42128dea68808c6a47589978e472c1c012b088
PtspluS <pierre.marez@isen.yncrea.fr>
2019-11-13 17:24:35 +0100

    constructor Mineur, Villageois, Bourgeois

--- src/Game/Personnages/Villageois.py
+++ src/Game/Personnages/Villageois.py
@@ -3,5 +3,8 @@ from Game.Personnages import Normal
 
 class Villageois(Normal.Normal):
 
-    def __init__(self):
-        pass
+    def __init__(self, img, nom, desc, vie=100, PO=50, posX=0, posY=0, bag=[], item=[], attaque=0, lvl=1, courage=1,
+                 PA=1):
+        super.__init__(img, nom, desc, vie=vie, PO=PO, posX=posX, posY=posY, bag=bag, item=item, attaque=attaque,
+                       lvl=lvl, courage=courage)
+        self.PA = PA
