3793bfe8bc802a989a21c7728a38fb392aa9ccca
Pierricles <pierromanuel5@gmail.com>
2019-11-19 16:49:54 +0100

    Change folder

--- Gameobject/Civil.py
+++
@@ -1,7 +0,0 @@
-from Game import PNJ
-
-
-class Civil(PNJ):
-
-    def __init__(self):
-        pass
--- Gameobject/Normal.py
+++
@@ -1,7 +0,0 @@
-from Game import Combattant
-
-
-class Normal(Combattant):
-
-    def __init__(self):
-        pass
